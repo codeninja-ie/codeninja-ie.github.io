afd1d38d8ff6abf5cc705399ed5caed37addcecf
liam <liam@xap.ie>
2018-02-22 12:43:35 +0000

    replaced register with submit form

--- scripts/index.js
+++ scripts/index.js
@@ -22,3 +22,16 @@ popupButton.addEventListener('click',function(event){
 if(/\?success=true$/.test(window.location.href)){
 	popupButton.parentNode.classList.remove('hide')
 }
+
+var individualSwitch = document.querySelector('#submission_type_individual');
+var groupSwitch = document.querySelector('#submission_type_group');
+var groupInfo = document.querySelector('#group-info');
+
+
+individualSwitch.addEventListener('click', function(event){
+  groupInfo.style.height = '0';
+});
+
+groupSwitch.addEventListener('click', function(event){
+  groupInfo.style.height = 'auto';
+});
